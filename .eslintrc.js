84b52216cd3b94c0e70d186756c7359ae6231111
Turbo <turbothinh@gmail.com>
2019-12-29 01:31:26 +0100

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
